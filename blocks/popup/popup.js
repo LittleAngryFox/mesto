c61ec60825ca6c75c53fa74f585973502432db4d
Ilina Elena <lenin.ok35@yandex.ru>
2020-07-11 23:59:11 +0700

    fix:Исправить ошибки после ревью

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,22 +1,23 @@
 let content = document.querySelector(".content");
 let popup = content.querySelector(".popup");
 
-let profile__info = content.querySelector(".profile__info"); //Информация о имени и сфере деятельности
-let profile__title = profile__info.querySelector(".profile__title");
-let profile__subtitle = profile__info.querySelector(".profile__subtitle");
+let profileInfo = content.querySelector(".profile__info"); //Информация о имени и сфере деятельности
+let profileTitle = profileInfo.querySelector(".profile__title");
+let profileSubtitle = profileInfo.querySelector(".profile__subtitle");
 
-let editButton = profile__info.querySelector(".profile__editButton"); //кнопка редактирования
+let editButton = profileInfo.querySelector(".profile__edit-button"); //кнопка редактирования
 
-let popup__container =  content.querySelector(".popup__container"); //форма редактирования
+let popupContainer =  content.querySelector(".popup__container"); //форма редактирования
+
+let nameInput = popup.querySelector("#heading");
+let jobInput = popup.querySelector("#subheading");
 
 //открытие popup
 function profileInfoEditOpened() {
   popup.classList.add("popup_opened"); //делает popup видимым
 
-  let heading_start = popup.querySelector("#heading"); //вставляем в форму "начальные" значения при открытии
-  let subheading_start = popup.querySelector("#subheading");
-  heading_start.setAttribute("value", profile__title.textContent);
-  subheading_start.setAttribute("value", profile__subtitle.textContent);
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
 }
 
 //закрытие popup
@@ -24,25 +25,20 @@ function profileInfoEditClosed() {
   popup.classList.remove("popup_opened");
 }
 
-//отслеживание нажатия на кнопку редактирования
-editButton.addEventListener("click", profileInfoEditOpened);
-
-
 function formSubmitHandler (evt) {
   evt.preventDefault();
 
-  //получаем новые значения
-  let nameInput = popup.querySelector("#heading");
-  let jobInput = popup.querySelector("#subheading");
-
-  profile__title.textContent = nameInput.value;
-  profile__subtitle.textContent = jobInput.value;
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
 
   //после сохранения закрываем popup
   profileInfoEditClosed();
 }
 
+
+//отслеживание нажатия на кнопку редактирования
+editButton.addEventListener("click", profileInfoEditOpened);
 //отслеживание отправки данных
-popup__container.addEventListener("submit", formSubmitHandler);
+popupContainer.addEventListener("submit", formSubmitHandler);
 //отслеживание закрытия окна
-popup__container.addEventListener("reset", profileInfoEditClosed);
+popupContainer.addEventListener("reset", profileInfoEditClosed);
